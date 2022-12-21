f49db10f0ae01ee9b210f5d7e19d4f43e78b780d
Alan Zou <itisalanlife@gmail.com>
2022-12-22 02:10:30 +0800

    test(app): fix init test

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -12,6 +12,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome use-mutative-project/gi)).toBeTruthy();
+    expect(getByText(/Welcome to UseMutative/gi)).toBeTruthy();
   });
 });
