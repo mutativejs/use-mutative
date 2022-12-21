6e330f17aa164d792b2249c536c6b14f86031c77
Alan Zou <itisalanlife@gmail.com>
2022-12-22 02:01:43 +0800

    feat(app): remove not need nx welcome

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import { UseMutative } from 'use-mutative';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="use-mutative-project" />
-      <div />
-    </>
+    <UseMutative />
   );
 }
 
