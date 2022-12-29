8b6dceec63e91bffb3c315718b5e1b1411c1232c
Alan Zou <itisalanlife@gmail.com>
2022-12-30 04:05:32 +0800

    fix(app): update example

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -10,7 +10,7 @@ export function App() {
   return (
     <div className="h-full grid grid-cols-12 p-12 gap-5">
       <nav className="col-span-full md:col-span-6 lg:col-span-5 flex flex-col">
-        <div className='flex-auto flex flex-col justify-end items-center'>
+        <div className="flex-auto flex flex-col justify-end items-center">
           <h1 className="text-6xl text-yellow-300 font-bold text-center">
             UseMutative
           </h1>
@@ -28,10 +28,8 @@ export function App() {
         <UseMutativeWithPatchesExample /> */}
         </div>
       </nav>
-      <main className="col-span-full md:col-span-6 lg:col-span-7 flex flex-col items-center justify-center overflow-hidden">
-        <div className="mt-4 w-full overflow-auto [&>pre]:!m-0 flex-auto">
-          <CodePreview code={useMutativeExampleCode} />
-        </div>
+      <main className="col-span-full md:col-span-6 lg:col-span-7 rounded overflow-hidden flex">
+        <CodePreview code={useMutativeExampleCode} />
       </main>
     </div>
   );
--- src/app/components/CodePreview.tsx
+++ src/app/components/CodePreview.tsx
@@ -12,21 +12,33 @@ type CodePreviewProps = {
 
 export const CodePreview: FC<CodePreviewProps> = ({ code }) => {
   return (
-    <Highlight {...defaultProps} code={code} language="tsx" theme={undefined}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        // define how each line is to be rendered in the code block,
-        // position is set to relative so the copy button can align to bottom right
-        <pre className={className} style={{ ...style, position: 'relative' }}>
-          <CopyButton className="absolute right-1 top-1" code={code} />
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
+    <div className="flex relative w-full">
+      <div className="w-full overflow-auto [&>pre]:!m-0">
+        <Highlight
+          {...defaultProps}
+          code={code}
+          language="tsx"
+          theme={undefined}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            // define how each line is to be rendered in the code block,
+            // position is set to relative so the copy button can align to bottom right
+            <pre
+              className={className}
+              style={{ ...style, position: 'relative' }}
+            >
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
               ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+            </pre>
+          )}
+        </Highlight>
+        <CopyButton className="absolute right-2 top-1" code={code} />
+      </div>
+    </div>
   );
 };
